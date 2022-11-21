b0909bb45e66d581891e9df51e6b78e75092a7c1
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2022-11-21 17:38:28 +0000

    carousel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
 
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 
     <!-- Javascript -->
     <script type="text/javascript" src={{ asset('js/app.js') }} defer></script>
--- resources/views/partials/post_item.blade.php
+++ resources/views/partials/post_item.blade.php
@@ -35,7 +35,26 @@
                 </div>
 
                 <!-- TODO: Ver imagens da database -->
-                <img src="/../post.jpg" class="img-fluid">
+                
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <img class="d-block w-100" src="../post.jpg" alt="Primeiro Slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="../logo.png" alt="Segundo Slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="../post.jpg" alt="Terceiro Slide">
+                    </div>
+                  </div>
+                  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev" style="filter: invert(100%);">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next" style="filter: invert(100%);">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  </a>
+                </div>
 
                 <div class="p-2">
                     <p class="text-justify">{{ $post->text }}</p>
