830b8c8e218c7a34e166535bcaaf9106b4701bba
up202006302 <up202006302@fe.up.pt>
2022-11-22 13:29:05 +0000

    main_features

--- resources/views/pages/features.blade.php
+++ resources/views/pages/features.blade.php
@@ -6,10 +6,54 @@
             <h2 class="pb-2 border-bottom">Main Features</h2>
             <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
 
-                @for ($i = 0; $i < 9; $i++)
+                
                     <div class="col d-flex align-items-start">
                         <div>
-                            <h3 class="fs-2">Featured title</h3>
+                            <h3 class="fs-2">Group</h3>
+                            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
+                                sentence and probably just keep going until we run out of words.</p>
+                            <a href="#" class="btn btn-primary">
+                                Group
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col d-flex align-items-start">
+                        <div>
+                            <h3 class="fs-2">Message</h3>
+                            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
+                                sentence and probably just keep going until we run out of words.</p>
+                            <a href="#" class="btn btn-primary">
+                                Message
+                            </a>
+                        </div>
+                    </div>
+                    
+                    <div class="col d-flex align-items-start">
+                        <div>
+                            <h3 class="fs-2">Friends</h3>
+                            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
+                                sentence and probably just keep going until we run out of words.</p>
+                            <a href="#" class="btn btn-primary">
+                               Friends
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col d-flex align-items-start">
+                        <div>
+                            <h3 class="fs-2">Posts</h3>
+                            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
+                                sentence and probably just keep going until we run out of words.</p>
+                            <a href="#" class="btn btn-primary">
+                                Home
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col d-flex align-items-start">
+                        <div>
+                            <h3 class="fs-2">Search</h3>
                             <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
                                 sentence and probably just keep going until we run out of words.</p>
                             <a href="#" class="btn btn-primary">
@@ -17,7 +61,41 @@
                             </a>
                         </div>
                     </div>
-                @endfor
+
+                    <div class="col d-flex align-items-start">
+                        <div>
+                            <h3 class="fs-2">Login/Logout</h3>
+                            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
+                                sentence and probably just keep going until we run out of words.</p>
+                            <a href="#" class="btn btn-primary">
+                                Primary button
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col d-flex align-items-start">
+                        <div>
+                            <h3 class="fs-2">Owners</h3>
+                            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
+                                sentence and probably just keep going until we run out of words.</p>
+                            <a href="#" class="btn btn-primary">
+                                Primary button
+                            </a>
+                        </div>
+                    </div>
+
+                    
+                    <div class="col d-flex align-items-start">
+                        <div>
+                            <h3 class="fs-2">Private/Public Profiles or Groups</h3>
+                            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
+                                sentence and probably just keep going until we run out of words.</p>
+                            <a href="#" class="btn btn-primary">
+                                Primary button
+                            </a>
+                        </div>
+                    </div>
+
 
 
             </div>
