962f6597ec26d58db6e86e38bb7a2fc3528a0482
up202006302 <up202006302@fe.up.pt>
2022-11-22 18:03:14 +0000

    Main features

--- resources/views/pages/features.blade.php
+++ resources/views/pages/features.blade.php
@@ -10,8 +10,7 @@
                     <div class="col d-flex align-items-start">
                         <div>
                             <h3 class="fs-2">Group</h3>
-                            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
-                                sentence and probably just keep going until we run out of words.</p>
+                            <p>In nexus you have a possibility to create a public group or a private and make posts in group.</p>
                             <a href="#" class="btn btn-primary">
                                 Group
                             </a>
@@ -21,8 +20,7 @@
                     <div class="col d-flex align-items-start">
                         <div>
                             <h3 class="fs-2">Message</h3>
-                            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
-                                sentence and probably just keep going until we run out of words.</p>
+                            <p>You can send some menssage to your friends or also every single public profile.</p>
                             <a href="#" class="btn btn-primary">
                                 Message
                             </a>
@@ -32,8 +30,7 @@
                     <div class="col d-flex align-items-start">
                         <div>
                             <h3 class="fs-2">Friends</h3>
-                            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
-                                sentence and probably just keep going until we run out of words.</p>
+                            <p>In nexus you can send request to be your friend to have a possibility to check every posts and send a mensagens to your friend.That is the only way you can see a privale profile</p>
                             <a href="#" class="btn btn-primary">
                                Friends
                             </a>
@@ -43,10 +40,9 @@
                     <div class="col d-flex align-items-start">
                         <div>
                             <h3 class="fs-2">Posts</h3>
-                            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
-                                sentence and probably just keep going until we run out of words.</p>
+                            <p>The user of nexus can do posts in profile our in groups with a photo, a simple text or both of them.</p>
                             <a href="#" class="btn btn-primary">
-                                Home
+                                Posts
                             </a>
                         </div>
                     </div>
@@ -54,10 +50,9 @@
                     <div class="col d-flex align-items-start">
                         <div>
                             <h3 class="fs-2">Search</h3>
-                            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
-                                sentence and probably just keep going until we run out of words.</p>
+                            <p>Nexus allows you to search every group,profile or a topic.The user click in search bar and writes what him likes to search.</p>
                             <a href="#" class="btn btn-primary">
-                                Primary button
+                                Search
                             </a>
                         </div>
                     </div>
@@ -65,10 +60,9 @@
                     <div class="col d-flex align-items-start">
                         <div>
                             <h3 class="fs-2">Login/Logout</h3>
-                            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
-                                sentence and probably just keep going until we run out of words.</p>
+                            <p>To have access all funcionalities you have to take account for this you have to login with you email and password associated at your account</p>
                             <a href="#" class="btn btn-primary">
-                                Primary button
+                                Login/Logout
                             </a>
                         </div>
                     </div>
@@ -76,10 +70,9 @@
                     <div class="col d-flex align-items-start">
                         <div>
                             <h3 class="fs-2">Owners</h3>
-                            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
-                                sentence and probably just keep going until we run out of words.</p>
+                            <p>In every group you have at least one owner and the owner can delete members and delete posts of the group.</p>
                             <a href="#" class="btn btn-primary">
-                                Primary button
+                               Owners
                             </a>
                         </div>
                     </div>
@@ -88,14 +81,31 @@
                     <div class="col d-flex align-items-start">
                         <div>
                             <h3 class="fs-2">Private/Public Profiles or Groups</h3>
-                            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another
-                                sentence and probably just keep going until we run out of words.</p>
+                            <p>On nexus we have visibility restrictions on both groups and profiles. If the visibility is private then only friends of that profile can see the content of that profile if it is a group only members of the group can see posts of that group.</p>
                             <a href="#" class="btn btn-primary">
-                                Primary button
+                               Profile/Groups
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col d-flex align-items-start">
+                        <div>
+                            <h3 class="fs-2">Register</h3>
+                            <p>To be a new member of the nexus family, you must first register with your name, email, password and date of birth.</p>
+                            <a href="#" class="btn btn-primary">
+                               Register
                             </a>
                         </div>
                     </div>
 
+                    <div class="col d-flex align-items-start">
+                        <div>
+                            <h3 class="fs-2">Likes/comments</h3>
+                            <p>Nexus allows you to like or comment on posts from your friends or groups. If the profile of the person posting the post is public or the group is public, you can also like or comment on these posts</p>
+                            <a href="#" class="btn btn-primary">
+                               Likes/comments
+                            </a>
+                        </div>
+                    </div>
 
 
             </div>
