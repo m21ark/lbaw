40a45aacf1b9eda1c4dbc21688c0319dbb8a8822
Marco <marco14lyoko@gmail.com>
2022-11-27 11:45:45 +0000

    Laravel should be exterminated

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,27 +4,33 @@ namespace App\Http\Controllers;
 
 use App\Models\Comment;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Client\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class CommentController extends Controller
 {
-    public function create($id_post, $text/* Request $request*/)
+    public function create($id_post, Request $request)
     {
+        // $this->authorize('create', Comment::class);
+        DB::table('comment')->insert([
+            'id_post' => $id_post,
+            'id_commenter' => Auth::user()->id,
+            'text' => $request->input('text'),
+        ]);
+    }
 
+    public function delete($id_comment)
+    {
+        $comment = Comment::find($id_comment);
+        $this->authorize('delete', $comment);
+        $comment->delete();
+    }
 
-        $out = new \Symfony\Component\Console\Output\ConsoleOutput();
-        $out->writeln('here: ' . $id_post);
-
-        $out->writeln('here0');
-
-
-        DB::table('comment')->insert(
-            [
-                'text' => $text,
-                'id_post' => $id_post,
-                'id_commenter' => Auth::user()->id,
-            ]
-        );
+    public function edit($id_comment, Request $request)
+    {
+        $comment = Comment::find($id_comment);
+        $this->authorize('edit', $comment);
+        $comment->text = $request->input('text');
+        $comment->save();
     }
 }
