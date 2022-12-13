cf39e2e425f8a7dbb30fac9bcd67585579eeb6cb
Marco <marco14lyoko@gmail.com>
2022-12-13 18:18:15 +0000

    Thinner feed & search filters

--- resources/views/partials/home_timeline.blade.php
+++ resources/views/partials/home_timeline.blade.php
@@ -1,28 +1,28 @@
 @auth
-    <div class="list-group list-group-checkable form-check d-flex p-3 flex justify-content-between text-bg-light"
+    <div class="list-group list-group-checkable form-check d-flex p-1 flex justify-content-between text-bg-light"
         id="feed_filter">
 
         <input class="list-group-item-check pe-none" onclick="updateFeed('for_you')" type="radio" name="feed_filter"
             id="feed_radio_foryou" value="">
-        <label class="list-group-item rounded-3 py-3" for="feed_radio_foryou">
+        <label class="list-group-item rounded-3" for="feed_radio_foryou">
             For you
         </label>
 
         <input class="list-group-item-check pe-none" onclick="updateFeed('viral')" type="radio" name="feed_filter"
             id="feed_radio_viral" value="" checked>
-        <label class="list-group-item rounded-3 py-3" for="feed_radio_viral">
+        <label class="list-group-item rounded-3" for="feed_radio_viral">
             Viral
         </label>
 
         <input class="list-group-item-check pe-none" onclick="updateFeed('friends')" type="radio" name="feed_filter"
             id="feed_radio_friends" value="">
-        <label class="list-group-item rounded-3 py-3" for="feed_radio_friends">
+        <label class="list-group-item rounded-3" for="feed_radio_friends">
             Friends
         </label>
 
         <input class="list-group-item-check pe-none" onclick="updateFeed('groups')" type="radio" name="feed_filter"
             id="feed_radio_groups" value="">
-        <label class="list-group-item rounded-3 py-3" for="feed_radio_groups">
+        <label class="list-group-item rounded-3" for="feed_radio_groups">
             Groups
         </label>
     </div>
--- resources/views/partials/profile_feed.blade.php
+++ resources/views/partials/profile_feed.blade.php
@@ -1,27 +1,27 @@
-<div class="list-group list-group-checkable form-check d-flex p-3 flex justify-content-between text-bg-light"
+<div class="list-group list-group-checkable form-check d-flex p-1 flex justify-content-between text-bg-light"
     id="feed_filter">
 
     <input class="list-group-item-check pe-none" onclick="updateFeed('Posts')" type="radio" name="feed_filter"
         id="feed_radio_posts" value="" checked="">
-    <label class="list-group-item rounded-3 py-3" for="feed_radio_posts">
+    <label class="list-group-item rounded-3 " for="feed_radio_posts">
         Posts
     </label>
 
     <input class="list-group-item-check pe-none" onclick="updateFeed('Comments')" type="radio" name="feed_filter"
         id="feed_radio_comments" value="">
-    <label class="list-group-item rounded-3 py-3" for="feed_radio_comments">
+    <label class="list-group-item rounded-3 " for="feed_radio_comments">
         Comments
     </label>
 
     <input class="list-group-item-check pe-none" onclick="updateFeed('Groups')" type="radio" name="feed_filter"
         id="feed_radio_groups" value="">
-    <label class="list-group-item rounded-3 py-3" for="feed_radio_groups">
+    <label class="list-group-item rounded-3 " for="feed_radio_groups">
         Groups
     </label>
 
     <input class="list-group-item-check pe-none" onclick="updateFeed('Likes')" type="radio" name="feed_filter"
         id="feed_radio_likes" value="">
-    <label class="list-group-item rounded-3 py-3" for="feed_radio_likes">
+    <label class="list-group-item rounded-3 " for="feed_radio_likes">
         Likes
     </label>
 
