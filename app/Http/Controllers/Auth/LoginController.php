d7dfb5996a03e0165dc5f6793da7b442a13d27d8
Marco <marco14lyoko@gmail.com>
2022-12-14 21:43:54 +0000

    Login with google done

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Laravel\Socialite\Facades\Socialite;
 
 class LoginController extends Controller
 {
@@ -45,5 +47,67 @@ class LoginController extends Controller
     public function home()
     {
         return redirect('login');
+    }
+
+    /**
+     * Redirect the user to the Google authentication page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    /**
+     * Obtain the user information from Google.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback()
+    {
+        $out = new \Symfony\Component\Console\Output\ConsoleOutput();
+        $out->writeln("START_1");
+
+        try {
+            $user = Socialite::driver('google')->user();
+            $out->writeln("START_2");
+        } catch (\Exception $e) {
+            return redirect('/login');
+            $out->writeln("START_3");
+        }
+
+        $out->writeln("START_4");
+
+        $out->writeln("START_6");
+
+        // check if they're an existing user
+        $existingUser = User::where('email', $user->email)->first();
+
+        $out->writeln("START_7");
+
+
+        if ($existingUser !== null) {
+            // log them in
+            $out->writeln("START_8");
+            auth()->login($existingUser);
+            $out->writeln("START_9");
+        } else {
+            $out->writeln("START_10");
+            // create a new user
+            $newUser                  = new User;
+            $out->writeln("START_11");
+            $newUser->name            = $user->name;
+            $newUser->email           = $user->email;
+            $out->writeln("START_12");
+            //$newUser->avatar          = $user->avatar;
+            //$newUser->avatar_original = $user->avatar_original;
+            $newUser->save();
+            $out->writeln("START_13");
+            auth()->login($newUser, true);
+            $out->writeln("START_14");
+        }
+        $out->writeln("START_15");
+        return redirect()->to('/home');
     }
 }
