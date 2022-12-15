664321cd01f5df81a64eebdfc8c468e474c0bb79
Johnny-droid <joaoricardoralves@gmail.com>
2022-12-15 19:36:16 +0000

    Added dropdown options for ordering

--- resources/views/partials/home_timeline.blade.php
+++ resources/views/partials/home_timeline.blade.php
@@ -1,31 +1,46 @@
 @auth
-    <div class="list-group list-group-checkable form-check d-flex p-1 flex justify-content-between text-bg-light"
-        id="feed_filter">
+    <div class="d-flex">
 
-        <input class="list-group-item-check pe-none" onclick="updateFeed('for_you')" type="radio" name="feed_filter"
-            id="feed_radio_foryou" value="">
-        <label class="list-group-item rounded-3" for="feed_radio_foryou">
-            For you
-        </label>
+        <div class="list-group list-group-checkable form-check d-flex p-1 flex justify-content-between align-items-center flex-fill text-bg-light"
+            id="feed_filter">
 
-        <input class="list-group-item-check pe-none" onclick="updateFeed('viral')" type="radio" name="feed_filter"
-            id="feed_radio_viral" value="" checked>
-        <label class="list-group-item rounded-3" for="feed_radio_viral">
-            Viral
-        </label>
+            <input class="list-group-item-check pe-none" onclick="updateFeed('for_you')" type="radio" name="feed_filter"
+                id="feed_radio_foryou" value="">
+            <label class="list-group-item rounded-3" for="feed_radio_foryou">
+                For you
+            </label>
 
-        <input class="list-group-item-check pe-none" onclick="updateFeed('friends')" type="radio" name="feed_filter"
-            id="feed_radio_friends" value="">
-        <label class="list-group-item rounded-3" for="feed_radio_friends">
-            Friends
-        </label>
+            <input class="list-group-item-check pe-none" onclick="updateFeed('viral')" type="radio" name="feed_filter"
+                id="feed_radio_viral" value="" checked>
+            <label class="list-group-item rounded-3" for="feed_radio_viral">
+                Viral
+            </label>
 
-        <input class="list-group-item-check pe-none" onclick="updateFeed('groups')" type="radio" name="feed_filter"
-            id="feed_radio_groups" value="">
-        <label class="list-group-item rounded-3" for="feed_radio_groups">
-            Groups
-        </label>
+            <input class="list-group-item-check pe-none" onclick="updateFeed('friends')" type="radio" name="feed_filter"
+                id="feed_radio_friends" value="">
+            <label class="list-group-item rounded-3" for="feed_radio_friends">
+                Friends
+            </label>
+
+            <input class="list-group-item-check pe-none" onclick="updateFeed('groups')" type="radio" name="feed_filter"
+                id="feed_radio_groups" value="">
+            <label class="list-group-item rounded-3" for="feed_radio_groups">
+                Groups
+            </label>
+
+            
+        </div>
+
+        <div class="orderFilter dropdown">
+            <button class="btn dropdownPostButton" type="button">&#9776;</button>
+            <div class="dropdown_menu" style="z-index: 200000" hidden>
+                <a class="dropdown-item"  href="#">Order by Likes</a>
+                <a class="dropdown-item"  href="#">Order by Date</a>
+            </div>
+        </div>
+    
     </div>
+
 @endauth
 
 
