39210edde6b2ac4878a4a8f78b6cf2f913b57b87
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2022-12-15 01:08:53 +0000

    Adding images

--- resources/views/partials/messages_list.blade.php
+++ resources/views/partials/messages_list.blade.php
@@ -17,28 +17,31 @@
             $separateBySender[0][0] = $messageInDay[0];
             $j = 0;
             for ($i=1; $i < count($messageInDay); $i++) { 
-                if ($messageInDay[i].id_sender !== $messageInDay[i-1].id_sender) {
+                if ($messageInDay[$i]->id_sender !== $messageInDay[$i-1]->id_sender) {
                     $j++;
                 }
-                $separateBySender[$j][] = $messageInDay[i];
+                $separateBySender[$j][] = $messageInDay[$i];
             }
         ?>
 
         @foreach ($separateBySender as $messages)
-            <div class="d-flex flex-row {{$message->id_sender === Auth::user()->id ? 'justify-content-start' : 'justify-content-end' }}}}">
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3-bg.webp"
-                  alt="avatar 1" style="width: 45px; height: 100%;">
+            <div class="d-flex flex-row {{$messages[0]->id_sender === Auth::user()->id ? 'justify-content-end' : 'justify-content-start' }}">
+                @if ($messages[0]->id_sender !== Auth::user()->id)
+                    <img src="/{{$messages[0]->sender->photo}}"
+                      alt="avatar 1" style="width: 45px; height: 100%;">
+                @endif
                 <div>
                     @foreach ($messages as $message)
                               <p class="small p-2 ms-3 mb-1 rounded-3" style="background-color: #f5f6f7;">{{$message->text}}</p>
                     @endforeach
                     <p class="small ms-3 mb-3 rounded-3 text-muted">{{Str::substr($message->date, 10, 10)}}</p>
                 </div>
+                @if ($message->id_sender === Auth::user()->id)
+                    <img src="/{{$message->sender->photo}}"
+                      alt="avatar 1" style="width: 45px; height: 100%;">
+                @endif
             </div>
         @endforeach
-
-
     @endforeach
-
 
 </div>
