73453f0ce3b91b7a821271d279053fa8d08e1ea9
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2022-12-27 01:16:22 +0000

    Tooltips on home page

--- resources/views/partials/home_timeline.blade.php
+++ resources/views/partials/home_timeline.blade.php
@@ -8,25 +8,25 @@
 
             <input class="list-group-item-check pe-none feed-filter" type="radio" name="feed_filter" id="feed_radio_foryou"
                 value="for_you">
-            <label class="list-group-item rounded-3" for="feed_radio_foryou" style="min-width:6em;">
+            <label class="list-group-item rounded-3" for="feed_radio_foryou" style="min-width:6em;" data-toggle="tooltip" data-placement="top" title="Viral posts and posts from friends that may be of interest to you">
                 For you
             </label>
 
             <input class="list-group-item-check pe-none feed-filter" type="radio" name="feed_filter" id="feed_radio_viral"
                 value="viral" checked>
-            <label class="list-group-item rounded-3" for="feed_radio_viral">
+            <label class="list-group-item rounded-3" for="feed_radio_viral" data-toggle="tooltip" data-placement="top" title="Viral posts right now">
                 Viral
             </label>
 
             <input class="list-group-item-check pe-none feed-filter" type="radio" name="feed_filter"
                 id="feed_radio_friends" value="friends">
-            <label class="list-group-item rounded-3" for="feed_radio_friends">
+            <label class="list-group-item rounded-3" for="feed_radio_friends" data-toggle="tooltip" data-placement="top" title="News from your friends">
                 Friends
             </label>
 
             <input class="list-group-item-check pe-none feed-filter" type="radio" name="feed_filter"
-                id="feed_radio_groups" value="groups">
-            <label class="list-group-item rounded-3" for="feed_radio_groups">
+                id="feed_radio_groups">
+            <label class="list-group-item rounded-3" for="feed_radio_groups" value="groups" data-toggle="tooltip" data-placement="top" title="Your group news">
                 Groups
             </label>
 
