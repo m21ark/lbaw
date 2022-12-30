c74b5e5b5d6e6e79dcb81a4276c9d9343ab495af
Marco <marco14lyoko@gmail.com>
2022-12-30 16:26:37 +0000

    Ricardo, im disapointed

--- resources/views/pages/report_item.blade.php
+++ resources/views/pages/report_item.blade.php
@@ -107,27 +107,7 @@
 
         <div id="reports_list">
             @foreach ($reports as $report)
-                <div class="card p-3 mb-4" id="reports_list_item_{{ $report->id }}">
-                    <h5 class="mb-4">Report description:</h5>
-                    <p>{{ $report->description }}</p>
-                    <a href="#!" class="position-absolute btn btn-outline-danger reject_user_report_btn"
-                        style="width: 20%;right:2em" data-reportid="{{ $report->id }}">Reject</a>
-
-
-                    <p><b>Made by: </b> <a class="text-decoration-none"
-                            href="/profile/{{ $report->reporter->username }}">{{ $report->reporter->username }}</a> on
-                        {{ $report->report_date }}</p>
-
-
-                    <h5 class="mt-3">Reported content:</h5>
-
-                    @if ($report->id_post === null)
-                        @include('partials.comment_item', ['comment' => $report->comment])
-                    @else
-                        @include('partials.post_item', ['post' => $report->post])
-                    @endif
-
-                </div>
+                @include('partials.report_pendent_item', ['report' => $report])
             @endforeach
         </div>
     </div>
@@ -141,34 +121,7 @@
         @endif
 
         @foreach ($decided_reports as $report)
-            <div class="card p-3 mb-4">
-                <h5 class="mb-4">Report description:</h5>
-                <p>{{ $report->description }}</p>
-
-
-                <p><b>Made by: </b> <a class="text-decoration-none"
-                        href="/profile/{{ $report->reporter->username }}">{{ $report->reporter->username }}</a> on
-                    {{ $report->report_date }}</p>
-
-                <h5>Decision information:</h5>
-                <ul>
-                    <li>Decision: {{ $report->decision }}</li>
-                    <li>Decision date: {{ $report->decision_date ?? 'N/A' }}</li>
-                    <li>Decision made by: {{ $report->avaliator->username ?? 'N/A' }}</li>
-                </ul>
-
-                <h5 class="mt-3">Reported content:</h5>
-
-                @if ($report->id_post === null)
-                    @include('partials.comment_item', ['comment' => $report->comment])
-                @else
-                    @include('partials.post_item', ['post' => $report->post])
-                @endif
-
-
-
-
-            </div>
+            @include('partials.report_decided_item', ['report' => $report])
         @endforeach
     </div>
 @endsection
