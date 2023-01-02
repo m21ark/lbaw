582c5df691423e33725bb494bfcbdf3c4b11f832
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2023-01-02 00:53:33 +0000

    More html validation

--- resources/views/pages/messages.blade.php
+++ resources/views/pages/messages.blade.php
@@ -33,7 +33,7 @@
                     </a>
                 </div>
             @else
-                <img src="/Illustration 18.png" alt="No Conversation Selected Art" style="width:100%"; id="default_img">
+                <img src="/Illustration_18.png" alt="No Conversation Selected Art" style="width:100%;" id="default_img">
             @endif
 
         </div>
